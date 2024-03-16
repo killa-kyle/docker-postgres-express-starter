7c75a84b19146863ea58a0790d855f4a100b369d
Kyle Rose <kylerose@gmail.com>
2024-03-16 18:37:26 -0400

    add limit and count to get query

--- knexfile.ts
+++ knexfile.ts
@@ -1,36 +1,4 @@
 require('dotenv-extended').load()
-import type { Knex } from "knex";
-
-
-// const config: { [key: string]: Knex.Config } = {
-//   // set knexfile for production
-//   development: {
-//     client: "pg",
-//     connection: process.env.DATABASE_URL,
-//     pool: {
-//       min: 2,
-//       max: 10
-//     },
-//     debug: true,
-//     migrations: {
-//       tableName: "knex_migrations"
-//     },
-//     seeds: {
-//       directory: './seeds/dev',
-//     },
-//   },
-//   production: {
-//     client: "pg",
-//     connection: process.env.DATABASE_URL,
-//     pool: {
-//       min: 2,
-//       max: 10
-//     },
-//     migrations: {
-//       tableName: "knex_migrations"
-//     }
-//   }
-// };
 
 const config = {
   client: "pg",
