5489aa012f6f89fd8a828501a9ad1048ec8cd9c6
Kyle Rose <kylerose@gmail.com>
2024-03-16 11:34:16 -0400

    clean up

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,11 @@
 import express from 'express'
 import helmet from 'helmet'
 
-// routes
 import routes from './routes'
 
 // core
 const app = express()
 app.use(helmet())
-// prefix all nested routes with /api/v1
-
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json({limit: '1mb'}))
